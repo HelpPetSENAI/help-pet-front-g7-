fff33fb47f77d9c04e2ac21527523befc40c618f
Nikidsy <dianaoliveirandrade@gmail.com>
2026-05-18 06:53:13 -0300

    style: update background visuals and decorations

--- src/components/Background/BackgroundDecoration.jsx
+++ src/components/Background/BackgroundDecoration.jsx
@@ -1,16 +1,18 @@
-import styed, { styled } from "styled-components"
-import decor from "../../assets/images/decor.svg"
+import styled from "styled-components";
+import decor from "../../assets/images/decor.svg";
 
 const Decor = styled.div`
-
   position: absolute;
-  inset: 0;
+  top: 0;
+  right: 0;
+  width: 60%;  
+  height: 100%;
 
-  background: url(${decor}) no-repeat right center;
-  background-size: 50%;
+  background: url(${decor}) no-repeat center;
+  background-size: cover;
+
   opacity: 0.25;
-
-  pointer-events: none; 
+  pointer-events: none;
 `;
 
 export default function BackgroundImages() {
